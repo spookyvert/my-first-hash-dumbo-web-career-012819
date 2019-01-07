542e5c62213aabffead6963613840bbdab2e7cf7
Shemar Dacosta <dshemar6@gmail.com>
2019-01-07 04:33:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash = {
+    "shemar" => 1,
+    "is" => 2,
+    "cool" => 3
+  }
   
 end
 
